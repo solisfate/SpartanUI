acfe50db063f33c6f2775e12dec05ee8fb75124c
wutname1 <jeremy12@gmail.com>
2026-03-04 19:39:15 -0600

    disable db reset on old profiles

--- Core/Framework.lua
+++ Core/Framework.lua
@@ -471,14 +471,14 @@ end
 
 function SUI:DBUpgrades()
 	-- Profiles from before 6.5.0 are too old to migrate - reset and re-setup
-	local storedVersion = SUI.DB.Version
-	if storedVersion and storedVersion ~= '0' and storedVersion ~= '' and storedVersion < '6.5.0' then
-		SUI:Print('Your profile is from a very old version (' .. storedVersion .. '). Resetting to defaults.')
-		SUI.SpartanUIDB:ResetProfile()
-		SUI.DB = SUI.SpartanUIDB.profile
-		SUI.DB.Version = SUI.Version
-		return
-	end
+	-- local storedVersion = SUI.DB.Version
+	-- if storedVersion and storedVersion ~= '0' and storedVersion ~= '' and storedVersion < '6.5.0' then
+	-- 	SUI:Print('Your profile is from a very old version (' .. storedVersion .. '). Resetting to defaults.')
+	-- 	SUI.SpartanUIDB:ResetProfile()
+	-- 	SUI.DB = SUI.SpartanUIDB.profile
+	-- 	SUI.DB.Version = SUI.Version
+	-- 	return
+	-- end
 
 	-- Access raw saved variables for namespace migration
 	-- Modules haven't registered their namespaces yet at this point,
